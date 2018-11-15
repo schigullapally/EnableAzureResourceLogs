e66b046d5a9ff776d60937f4a2aad7be1eb4f51d
Sandeep Chigullapally <chsk@outlook.com>
2018-11-15 17:48:25 -0500

    Add files via upload
    This script will turn on the diagnostic logs of Azure resources in specific subscription and stream them to OMS workspace (OMS workspace can be in any subscription).
    1. Diagnostic logs are divided into two types: 'Logs' and 'All Metrics'
    2. Script will first turn on "All Metrics" and then it will turn on "Logs"
    3. If "All Metrics" doesn't exist then it will not turn on the "Logs"
    4. Script will additionally turn on NSG logs though "All Metrics" doesn't exists for them

---
+++ EnableAzureResourcesLogsToOMS.ps1
@@ -0,0 +1,84 @@
+﻿$workspaceid = "/subscriptions/gg7b9b1c-1234-4049-88b6-ff1234567/resourcegroups/nameoftheresourcegroup/providers/microsoft.operationalinsights/workspaces/nameoftheworkspace"
+$subid = "subscriptionid where the resources belong to"
+
+# Login to Azure - if already logged in, use existing credentials.
+Write-Host "Authenticating to Azure..." -ForegroundColor Cyan
+try
+{
+    $AzureLogin = Get-AzureRmSubscription 
+}
+catch
+{
+    $null = Login-AzureRmAccount
+    $AzureLogin = Get-AzureRmSubscription
+}
+
+# Authenticate to Azure if not already authenticated 
+# Ensure this is the subscription where your Azure Resources are you want to send diagnostic data from
+If($AzureLogin)
+{
+   
+    $SubscriptionID = Get-AzureRmSubscription -SubscriptionId $subid | Select-Object SubscriptionId
+
+    # Get Subscriptions under the logged in account
+        $ResourcesToCheck = @()
+        $getnameofdiagnosticsettings = @()
+        Write-Host "Selecting Azure Subscription: $($SubscriptionID.SubscriptionID) ..." -ForegroundColor Cyan
+        $NULL = Select-AzureRmSubscription -SubscriptionId $($SubscriptionID.SubscriptionID)
+        $ResourcesToCheck = Get-AzureRmResource | select-object ResourceId, ResourceType
+        write-host "number of resources " $ResourcesToCheck.Count
+        
+        foreach ($resource in $ResourcesToCheck)
+        {
+
+        $resourceidtoenable = $resource.ResourceId
+
+         write-host "$($resourceidtoenable)"
+        
+            
+                    $getnameofdiagnosticsettings = Get-AzureRmDiagnosticSetting -ResourceId $resourceidtoenable | select-object Name
+
+                    foreach($diagtoremove in $getnameofdiagnosticsettings)
+                    {
+                        #Remove-AzureRmDiagnosticSetting -ResourceId "$($resourceidtoenable)" -Name $diagtoremove.Name
+                        Set-AzureRmDiagnosticSetting -ResourceId $resourceidtoenable -Name $diagtoremove.Name -Enabled $false
+                    }
+                    
+                    write-host $resource.ResourceType.Split("/")[-1]
+
+                    if ($resource.ResourceType.Split("/")[-1] -eq "networkSecurityGroups")
+                    {
+                        Set-AzureRmDiagnosticSetting -Name "Logstooms" -ResourceId $resourceidtoenable -WorkspaceId $workspaceid -Enabled $True -Categories "NetworkSecurityGroupEvent","NetworkSecurityGroupRuleCounter"
+                    }
+
+                    Set-AzureRmDiagnosticSetting -Name "Metricstooms" -ResourceId $resourceidtoenable -WorkspaceId ($workspaceid) -Enabled $True -MetricCategory "AllMetrics"
+
+                    $setting = Get-AzureRmDiagnosticSetting -ResourceId $resourceidtoenable
+                    
+                    if ($setting.Logs) 
+                        { 
+                            $Categories = $setting.Logs.category
+
+                            foreach ($cat in $categories) 
+                            
+                            {
+
+                                write-host $cat
+                                
+                                Set-AzureRmDiagnosticSetting -Name "Logstooms" -ResourceId $resourceidtoenable -WorkspaceId ($workspaceid) -Enabled $True -Categories $cat
+
+
+
+                            }
+                        
+                        }
+                   
+                
+          
+
+        }
+        
+ 
+
+
+}
